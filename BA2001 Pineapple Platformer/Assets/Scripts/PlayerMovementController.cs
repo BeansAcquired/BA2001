42fdac3cd166b05736719490eeb49fe6ff77224a
Dakotah Voet <dakotahvoet@gmail.com>
2020-08-18 00:26:53 -0500

    Player movement (cont.)
    Fixed jump
    Added Trace logger

--- BA2001 Pineapple Platformer/Assets/Scripts/PlayerMovementController.cs
+++ BA2001 Pineapple Platformer/Assets/Scripts/PlayerMovementController.cs
@@ -1,19 +1,40 @@
-﻿using System.Collections;
+﻿using BAUnity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
     #region Properties
-    // Public
-    public float Speed;
-    public float JumpForce;
-    
+    [SerializeField] private LayerMask GroundLayer;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform GroundCheck;                           // A position marking where to check if the player is grounded.
 
     // Private
     private Rigidbody2D _rigidbody;
     private float _horizontalMove = 0f;
-    private bool isJumping = false;
+    private bool _isJumping = false;
+    private bool _isSprinting = false;
+    private bool _isGrounded;
+    private bool _canJump = false;
+
+
+    private float Speed
+    {
+        get
+        {
+            return _isSprinting
+                ? _baseSpeed * _sprintMultiplier
+                : _baseSpeed;
+        }
+    }
+
+    // Configuration
+    public float _jumpForce = 40f;
+    private float _baseSpeed = 40f;
+    private float _sprintMultiplier = 1.5f; // Speed multiplier while sprinting
+    private float _groundedRadius = .2f; // Radius of the circle to determine if grounded
+    private float _fallMultiplier = 2.5f; // Radius of the circle to determine if grounded
+    private float _lowJumpMultiplier = 2f; // Radius of the circle to determine if grounded
 
     [Range(0, .3f)]
     [SerializeField]
@@ -24,6 +45,7 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Start()
     {
+        Trace.Info("Start", this);
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -31,37 +53,59 @@ public class PlayerMovementController : MonoBehaviour
     private void Update()
     {
         _horizontalMove = Input.GetAxisRaw("Horizontal") * Speed;
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            isJumping = true;
+            _isJumping = true;
         }
+        _isSprinting = Input.GetButton("Sprint");
+       
     }
 
     // Apply physics in FixedUpdate
     private void FixedUpdate()
     {
-        // TODO: Add debug logging
-        // TODO: Add isGrounded check
+        CheckGrounded();
         Move();
-        Jump();
     }
 
     private void Move()
     {
+
         // Find target velocity
         Vector3 targetVelocity = new Vector2(_horizontalMove * Time.fixedDeltaTime * 10f, _rigidbody.velocity.y);
 
         // Apply and smooth
         _rigidbody.velocity = Vector3.SmoothDamp(_rigidbody.velocity, targetVelocity, ref _velocity, _movementSmoothing);
+
+        if(_isGrounded && _isJumping)
+        {
+            Trace.Log("Jump!");
+            _rigidbody.AddForce(new Vector2(0f, _jumpForce * 10f));
+            if (_rigidbody.velocity.y < 0)
+            {
+                _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (_fallMultiplier - 1) * Time.fixedDeltaTime;
+            }
+            else if (_rigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (_lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+            }
+            _isJumping = false;
+        }
     }
 
-    private void Jump()
+    private void CheckGrounded()
     {
-        if (isJumping)
-        {
-            _rigidbody.AddForce(new Vector2(0f, JumpForce * 10f));
-            isJumping = false;
+        //bool wasGrounded = _isGrounded;
+        _isGrounded = false;
 
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, _groundedRadius, GroundLayer);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                _isGrounded = true;
+            }
         }
     }
 }
--- BA2001 Pineapple Platformer/Assets/Scripts/Utility/TraceService.cs
+++ BA2001 Pineapple Platformer/Assets/Scripts/Utility/TraceService.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using BAUnity;
 
-public class GameManager : MonoBehaviour
+public class TraceService : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Trace.Info("Start", this);
+        
     }
 
     // Update is called once per frame
